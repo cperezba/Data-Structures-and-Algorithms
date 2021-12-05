09e31742b4b3d5ae3045c01226a913595379c072
Carlos Perez <ca.perez.barr@gmail.com>
2021-12-05 10:01:42 -0600

    Initial Commit

---
+++ DSAArrays.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class DSAArrays {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[3];
+
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+
+        
+
+        System.out.println(Arrays.toString(numbers));
+    }
+
+}

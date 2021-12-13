b819dae97320f739dbff24b07a32b7b55665092c
Carlos Perez <ca.perez.barr@gmail.com>
2021-12-13 15:04:53 -0600

    Update: Transferred to full Java Program

--- DSAArrays.java
+++ DSAArrays.java
@@ -1,17 +1,22 @@
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class DSAArrays {
 
     public static void main(String[] args) {
-        int [] numbers = new int[3];
+         int [] numbers = new int[3];
 
-        numbers[0] = 10;
-        numbers[1] = 20;
-        numbers[2] = 30;
+         numbers[0] = 10;
+         numbers[1] = 20;
+         numbers[2] = 30;
 
-        
 
         System.out.println(Arrays.toString(numbers));
+
+
+
     }
 
 }

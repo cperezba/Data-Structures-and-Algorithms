1cdc5a7b0940924b6b16dc09f7deabf969a39d67
Carlos Perez <ca.perez.barr@gmail.com>
2021-12-14 00:22:43 -0600

    Update: Array class was added.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,17 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int [] numbers = new int[3];
+//        int [] numbers = new int[3];
+//
+//        numbers[0] = 10;
+//        numbers[1] = 20;
+//        numbers[2] = 30;
+//
+//        System.out.println(Arrays.toString(numbers));
 
-        numbers[0] = 10;
-        numbers[1] = 20;
-        numbers[2] = 30;
+    Array numbers = new Array(3);
 
-        System.out.println(Arrays.toString(numbers));
+    numbers.print();
+
     }
 }
